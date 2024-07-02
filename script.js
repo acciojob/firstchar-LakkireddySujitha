02440ce2134e08f896abe591b88a1b157e36caaa
LakkireddySujitha <lakkireddy.sujitha1234@gmail.com>
2024-07-02 07:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstChar(text) {
   // your code here
+	const trimmedText = text.trim();
+if (trimmedText.length === 0) {
+    return '';
+  }
+  return trimmedText[0];
 }
 
 // Do not change the code below
